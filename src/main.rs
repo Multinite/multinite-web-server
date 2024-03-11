2fb36d4e7a72571d29ee99cb28b46461ab07f9db
Multinite <founders.multinite@gmail.com>
2024-03-11 11:05:21 +1000

    Updated webserver

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::net::{TcpListener, TcpStream};
 mod util;
 
 fn main() -> io::Result<()> {
+
+    let mut file = File::open("smaller_file.txt")?;
+
     // Bind the server to a socket address
     let listener = TcpListener::bind("127.0.0.1:2828")?;
 
@@ -17,7 +20,7 @@ fn main() -> io::Result<()> {
             // Handle each incoming connection
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr()?);
-                handle_client(stream)?;
+                handle_client(stream, file)?;
             }
             // Handle errors
             Err(e) => {
@@ -28,26 +31,15 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> io::Result<()> {
-
-    // let mut buffer = [0; 1024];
-    // let bytes_read = stream.read(&mut buffer)?;
-    // let request = util::decompress_string(&buffer[..bytes_read]);
-    // println!("Received request: {}", request);
-
-    // Open a file or any other data source
-    let mut file = File::open("smaller_file.txt")?;
+fn handle_client(stream: TcpStream, mut file: File) -> io::Result<()> {
 
     let mut encoder = GzEncoder::new(&stream, Compression::default());
 
     io::copy(&mut file, &mut encoder)?;
 
-    // Flush the encoder to ensure all data is compressed
     encoder.finish()?;
 
-    // Buffer to hold chunks of data
     println!("Response sent");
 
     Ok(())
 }
-// let content = &mws_compiler::parse_line("let x:  = 0;");
