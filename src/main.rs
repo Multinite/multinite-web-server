b20f9d577261b38a8abd99bc279c66938f32ac93
Multinite <founders.multinite@gmail.com>
2024-03-26 23:03:29 +1000

    Chore: Prettified

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::fs::File;
@@ -8,10 +7,9 @@ use std::net::{TcpListener, TcpStream};
 mod util;
 
 fn main() -> io::Result<()> {
-
-
     // Bind the server to a socket address
     let listener = TcpListener::bind("127.0.0.1:2828")?;
+    println!("Server running on port 2828");
 
     // Listen for incoming connections
     for stream in listener.incoming() {
@@ -31,7 +29,6 @@ fn main() -> io::Result<()> {
 }
 
 fn handle_client(stream: TcpStream, mut file: File) -> io::Result<()> {
-
     let mut encoder = GzEncoder::new(&stream, Compression::default());
 
     io::copy(&mut file, &mut encoder)?;
